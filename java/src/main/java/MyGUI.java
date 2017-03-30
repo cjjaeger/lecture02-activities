b897897349e76a26fc1296ebe6d0d9957c7c25ff
Christa Joy Jaeger <cjjaeger@uw.edu>
2017-03-30 14:57:35 -0700

    lecture

--- java/src/main/java/MyGUI.java
+++ java/src/main/java/MyGUI.java
@@ -13,15 +13,33 @@ public class MyGUI extends JFrame {
 		
 		JButton theButton = new JButton("Click me!");
 
+		theButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				System.out.println("click");
+			}
+		});
+		JButton theButton1 = new JButton("Don't Click!");
 
-
-
-		
-		this.add(theButton);
-		
+		theButton1.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				System.out.println("what have you done!");
+			}
+		});
+		JPanel container = new JPanel();
+		container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
+		container.add(theButton);
+		container.add(theButton1);
+		JPanel other = new JPanel();
+		other.setLayout(new BoxLayout(other, BoxLayout.X_AXIS));
+		other.add(new JButton("A"));
+		other.add(new JButton("B"));
+		other.add(new JButton("C"));
+		this.add(container);
+		this.add(other);
 		this.pack();
 		this.setVisible(true);		
 	}
+	
 	
 	public static void main(String[] args) {
     new MyGUI(); //create the frame
